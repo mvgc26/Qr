ed134134fcafdf87d6421ba9800121cb80e97356
Maria Garcia <mariav2601@gmail.com>
2023-10-29 12:04:43 -0400

    update

--- qr_code.py
+++ qr_code.py
@@ -2,12 +2,12 @@ import qrcode
 
 text = "esto hace kabum puffff"
 
-qr = qrcode.QRCode(version = 1, box_size = 10, border = 2)
+qr = qrcode.QRCode(version=1, box_size=10, border=2)
 
 qr.add_data(text)
 
 qr.make(fit=True)
 
-imagen_qr = qr.make_image(fill_color = "black", back_color="white")
+imagen_qr = qr.make_image(fill_color="black", back_color="white")
 
 imagen_qr.save("codigo_qr.png")
